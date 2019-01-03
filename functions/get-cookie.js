b73e89cfdf366d78e0059e260c2983ab683d877f
divya <hello@shortdiv.com>
2019-01-03 11:07:55 -0600

    does this work

--- functions/get-cookie.js
+++ functions/get-cookie.js
@@ -1,7 +1,4 @@
 exports.handler = function(event, context, callback) {
   console.log("hello");
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ token: "hi" })
-  });
+  callback(null, { statusCode: 200, body: "Hello, World" });
 };
