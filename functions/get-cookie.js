4d5d9b5f33ce48865c5267bf47aafef75382c3ac
divya <hello@shortdiv.com>
2019-01-03 09:57:56 -0600

    testing function

--- functions/get-cookie.js
+++ functions/get-cookie.js
@@ -2,21 +2,9 @@ const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 
 exports.handler = function(event, context, callback) {
-  const { headers } = event;
-  const cookieHeader = headers.cookie || "";
-  const cookies = cookie.parse(cookieHeader);
-
-  let decodedToken;
-  try {
-    decodedToken = jwt.decode(cookies.nf_jwt, { complete: true });
-    console.log(decodedToken);
-  } catch (e) {
-    console.log(e);
-  }
+  console.log(event);
   return callback(null, {
     statusCode: 200,
-    body: JSON.stringify({
-      token: decodedToken
-    })
+    body: JSON.stringify({})
   });
 };
