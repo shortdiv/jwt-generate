fb93a0afa2b4022afd42b9d9e80f2f82c1f8d675
divya <hello@shortdiv.com>
2019-01-08 13:23:22 -0600

    add redirect logic

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,10 +1,70 @@
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const parseURL = require("url-parse");
+
 exports.handler = function(event, context, callback) {
   const params = event.queryStringParameters;
+  const urlData = parseURL(params.site);
+  const redirectBaseUrl = urlData.origin;
+  const redirectUrl = urlData.href;
 
-  console.log(params.site);
+  //check that cookies are present
+  const { headers } = event;
+  const cookieHeader = headers.cookie || "";
+  const cookies = cookie.parse(cookieHeader);
+
+  if (cookieHeader === "" || !cookies.nf_jwt) {
+    const redirectToURL = redirectUrl
+      ? `${siteUrl}?site=${redirectUrl}`
+      : siteUrl;
+    return callback(null, {
+      statusCode: 302,
+      headers: {
+        Location: redirectToURL,
+        "Cache-Control": "no-cache"
+      },
+      body: JSON.stringify({ message: "cookie is not present" })
+    });
+  }
+
+  let decodedToken, roles, message, originalToken;
+  try {
+    originalToken = cookies.nf_jwt;
+    decodedToken = jwt.decode(cookies.nf_jwt, { complete: true });
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (decodedToken === null) {
+    return callback(null, {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: `Your token is invalid. Logout and log back in`
+      })
+    });
+  }
+
+  const newTokenData = {
+    sub: decodedToken.payload.sub,
+    exp: decodedToken.payload.exp,
+    email: decodedToken.payload.email,
+    app_metadata: {
+      ...decodedToken.payload.app_metadata,
+      authorization: {
+        roles: ["admin", "editor"]
+      }
+    },
+    user_metadata: decodedToken.payload.user_metadata
+  };
+
+  const newToken = jwt.sign(newTokenData, "suchSecretsMuchToHide");
 
   callback(null, {
-    statusCode: 200,
+    statusCode: 302,
+    headers: {
+      Location: `${redirectBaseUrl}/.netlify/functions/set-cookie?token=${newToken}&url=${redirectUrl}`,
+      "Cache-Control": "no-cache"
+    },
     body: JSON.stringify({ message: "hello" })
   });
 };
